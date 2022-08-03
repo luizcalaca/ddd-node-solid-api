0fd359a854a35f96e00fc07a3564f738b28b8d4e
Luiz Calaça <luizcalaca@gmail.com>
2022-08-03 09:24:30 -0300

    refactor: adding fields on constructor for initialization

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -7,9 +7,9 @@ export class User {
     public password: string
 
     constructor(props: Omit<User, 'id'>, id?: string) {
-        this.name = ''
-        this.email = ''
-        this.password = ''
+        this.name = props.name
+        this.email = props.email
+        this.password = props.password
 
         if (!id)
             this.id = v4()
